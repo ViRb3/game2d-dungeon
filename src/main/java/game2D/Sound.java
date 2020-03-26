96a74631aebaef323602cbfd7ba19335cc3d1286
Victor <virb3e@gmail.com>
2020-03-26 11:45:10 +0200

    Rework Sound

--- src/main/java/game2D/Sound.java
+++ src/main/java/game2D/Sound.java
@@ -1,42 +1,62 @@
 package game2D;
 
 import javax.sound.sampled.*;
+import java.io.FilterInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Sound extends Thread {
+public class Sound {
+    private static ExecutorService playExecutor = Executors.newFixedThreadPool(4);
 
-    String filename;    // The name of the file to play
-    boolean finished;    // A flag showing that the thread has finished
+    public boolean finished;
+    private Clip clip;
 
-    public Sound(String fname) {
-        filename = fname;
-        finished = false;
-    }
-
-    /**
-     * run will play the actual sound but you should not call it directly. You need to call the 'start' method of your
-     * sound object (inherited from Thread, you do not need to declare your own). 'run' will eventually be called by
-     * 'start' when it has been scheduled by the process scheduler.
-     */
-    public void run() {
+    public Sound(String path) {
         try {
-            try (InputStream s = Util.readResource(filename)) {
-                AudioInputStream stream = AudioSystem.getAudioInputStream(s);
-                AudioFormat format = stream.getFormat();
-                DataLine.Info info = new DataLine.Info(Clip.class, format);
-                Clip clip = (Clip) AudioSystem.getLine(info);
-                clip.open(stream);
-                clip.start();
-                Thread.sleep(100);
-                while (clip.isRunning()) {
-                    Thread.sleep(100);
-                }
-                clip.close();
-            }
-        } catch (Exception e) {
+            InputStream s = Util.readResource(path);
+            AudioInputStream stream = AudioSystem.getAudioInputStream(s);
+            AudioFormat format = stream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            clip = (Clip) AudioSystem.getLine(info);
+            clip.open(stream);
+        } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
             e.printStackTrace();
         }
         finished = true;
+    }
 
+    public Sound(FilterInputStream filter) {
+        try {
+            AudioInputStream stream = AudioSystem.getAudioInputStream(filter);
+            AudioFormat format = stream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            clip = (Clip) AudioSystem.getLine(info);
+            clip.open(stream);
+        } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        }
+        finished = true;
+    }
+
+    public void play() {
+        if (finished)
+            playExecutor.submit(this::doPlay);
+    }
+
+    private void doPlay() {
+        finished = false;
+        clip.start();
+        try {
+            while (clip.getMicrosecondPosition() < clip.getMicrosecondLength()) {
+                Thread.sleep(10);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        clip.stop();
+        clip.setFramePosition(0);
+        finished = true;
     }
 }
