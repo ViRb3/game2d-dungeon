fd0b0f977250a84485e304602d32496050c94495
Victor <virb3e@gmail.com>
2020-03-26 15:54:52 +0200

    Add merge.py content

---
+++ merge.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from PIL import Image
+
+images= [Image.open(file) for file in os.listdir('.') if file.endswith('.png')]
+widths, heights = zip(*(i.size for i in images))
+
+total_width = sum(widths)
+max_height = max(heights)
+
+new_im = Image.new('RGBA', (total_width, max_height))
+
+x_offset = 0
+for im in images:
+  new_im.paste(im, (x_offset,0))
+  x_offset += im.size[0]
+
+new_im.save('tileset.png')
